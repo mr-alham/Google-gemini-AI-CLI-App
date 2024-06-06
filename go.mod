d75c7a375897d5609747ad2cc05ce314d2cb9fe9
alham_manjaro <86831608+mr-alham@users.noreply.github.com>
2024-06-06 23:44:40 +0530

    Done UI working without a problem

---
+++ go.mod
@@ -0,0 +1,62 @@
+module github.com/mr-alham/Google-Gemini-AI-on-the-Terminal
+
+go 1.22.3
+
+require (
+	github.com/atotto/clipboard v0.1.4
+	github.com/charmbracelet/glamour v0.7.0
+	github.com/google/generative-ai-go v0.13.0
+	golang.org/x/crypto v0.23.0
+	google.golang.org/api v0.182.0
+)
+
+require (
+	cloud.google.com/go v0.114.0 // indirect
+	cloud.google.com/go/ai v0.5.0 // indirect
+	cloud.google.com/go/auth v0.4.2 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.2 // indirect
+	cloud.google.com/go/compute/metadata v0.3.0 // indirect
+	cloud.google.com/go/longrunning v0.5.7 // indirect
+	github.com/alecthomas/chroma/v2 v2.8.0 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/dlclark/regexp2 v1.4.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/s2a-go v0.1.7 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.4 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.26 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/yuin/goldmark v1.5.4 // indirect
+	github.com/yuin/goldmark-emoji v1.0.2 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.51.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.51.0 // indirect
+	go.opentelemetry.io/otel v1.26.0 // indirect
+	go.opentelemetry.io/otel/metric v1.26.0 // indirect
+	go.opentelemetry.io/otel/trace v1.26.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/oauth2 v0.20.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/term v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/genproto v0.0.0-20240401170217-c3f982113cda // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240513163218-0867130af1f8 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240521202816-d264139d666e // indirect
+	google.golang.org/grpc v1.64.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
+)
--- main.go
+++ main.go
@@ -9,7 +9,12 @@ import (
 	"os"
 	"strings"
 
+	"github.com/charmbracelet/glamour"
+	"github.com/charmbracelet/glamour/ansi"
 	"github.com/google/generative-ai-go/genai"
+	"golang.org/x/crypto/ssh/terminal"
+
+	"github.com/atotto/clipboard"
 	"google.golang.org/api/option"
 )
 
@@ -33,8 +38,19 @@ type Config struct {
 }
 
 func main() {
+	width := terminalWidth()
+	titleA := "---------------🝔---------------"
+	title := "Gemini-AI on Terminal"
+
+	spaceA := strings.Repeat(" ", ((width - 31) / 2))
+	spaceTitle := strings.Repeat(" ", ((width - 22) / 2))
+
+	fmt.Println("\033[1;38;5;178m" + spaceA + titleA + "\033[0;37m")
+	fmt.Println("\033[1;38;5;38m" + spaceTitle + title + "\033[0m")
+	fmt.Println("\033[1;38;5;178m" + spaceA + titleA + "\033[0;37m")
+
 	// if you intend to use a different file for json specify it here
-	const configFile = "keys.json"
+	const configFile = "Gemini_Ai_Config/keys.json"
 
 	file, err := os.Open(configFile)
 	if err != nil {
@@ -62,8 +78,8 @@ func main() {
 	model.SetTemperature(config.GENERATION_CONFIG.Temperature)
 	model.SetTopP(config.GENERATION_CONFIG.Top_p)
 	model.SetTopK(config.GENERATION_CONFIG.Top_k)
-	model.ResponseMIMEType = config.GENERATION_CONFIG.Response_mime_type
 	model.MaxOutputTokens = config.GENERATION_CONFIG.Max_output_tokens
+	model.ResponseMIMEType = config.GENERATION_CONFIG.Response_mime_type
 	model.SystemInstruction = &genai.Content{
 		Parts: []genai.Part{genai.Text(config.SYSTEM_INSTRUCTION)},
 	}
@@ -122,18 +138,33 @@ func createClient(ctx context.Context, apiKey string) (*genai.Client, error) {
 
 // Generate text from text-and-image input (multimodal)
 func generateTextFromImage(ctx context.Context, model *genai.GenerativeModel) error {
+	width := terminalWidth()
+
+	fmt.Println("\033[2;1;93mYou are currently using Multi Mode Model")
+	fmt.Println("Enter `Text Mode` instead of image path, To switch to text-to-text model\033[0;1;2;95m")
+	fmt.Println(strings.Repeat("─", width-3), "\033[0;37m")
+
 	var pathToImage string
 	var userPrompt string
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-		fmt.Print("Path to The Image (OR `Text Mode` to switch): ")
+		fmt.Print("\033[0;1;38;5;28mPath to Image: \033[0;38;5;254m")
 		scanner.Scan()
 		pathToImage = scanner.Text()
 
 		if err := scanner.Err(); err != nil {
 			fmt.Println("Error scanning file: ", err)
 			continue
+		}
+
+		if pathToImage == "" {
+			clipboardContent, err := clipboard.ReadAll()
+			if err != nil {
+				fmt.Println("Error Reading Clipboard: ", err)
+				continue
+			}
+			pathToImage = clipboardContent
 		}
 
 		if strings.ToLower(pathToImage) == "text mode" {
@@ -146,7 +177,7 @@ func generateTextFromImage(ctx context.Context, model *genai.GenerativeModel) er
 			continue
 		}
 
-		fmt.Print("The Prompt: ")
+		fmt.Print("\033[0;1;38;5;28mThe Prompt: \033[0;38;5;254m")
 		scanner.Scan()
 		userPrompt = scanner.Text()
 
@@ -171,6 +202,12 @@ func generateTextFromImage(ctx context.Context, model *genai.GenerativeModel) er
 
 // Generate text from text-only input
 func generateTextFromPrompt(ctx context.Context, model *genai.GenerativeModel) error {
+	width := terminalWidth()
+
+	fmt.Println("\033[2;1;93mYou are currently using Text-to-Text Model")
+	fmt.Println("Enter `Image Mode` to switch to multi mode model\033[0;1;2;95m")
+	fmt.Println(strings.Repeat("─", width-3), "\033[0;37m")
+
 	var userPrompt string
 	scanner := bufio.NewScanner(os.Stdin)
 
@@ -178,7 +215,7 @@ func generateTextFromPrompt(ctx context.Context, model *genai.GenerativeModel) e
 	cs.History = []*genai.Content{}
 
 	for {
-		fmt.Print("The Prompt(Or `Image Mode` to switch): ")
+		fmt.Print("\033[0;1;38;5;28mThe Prompt: \033[0;38;5;254m")
 		scanner.Scan()
 		userPrompt = scanner.Text()
 
@@ -190,7 +227,7 @@ func generateTextFromPrompt(ctx context.Context, model *genai.GenerativeModel) e
 			fmt.Println("The Prompt is empty.")
 			continue
 		} else if strings.ToLower(userPrompt) == "image mode" {
-			generateTextFromPrompt(ctx, model)
+			generateTextFromImage(ctx, model)
 		}
 
 		resp, err := cs.SendMessage(ctx, genai.Text(userPrompt))
@@ -203,13 +240,284 @@ func generateTextFromPrompt(ctx context.Context, model *genai.GenerativeModel) e
 }
 
 func printResponse(resp *genai.GenerateContentResponse) {
+	width := terminalWidth()
+
+	const (
+		defaultMargin     = 2
+		defaultListIndent = 2
+	)
+
+	// DarkStyleConfig is the default dark style.
+	customStylingConfig := ansi.StyleConfig{
+		Document: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				BlockPrefix: "\n",
+				BlockSuffix: "\n",
+				Color:       stringPtr("250"),
+			},
+			Margin: uintPtr(defaultMargin),
+		},
+		BlockQuote: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{},
+			Indent:         uintPtr(2),
+			IndentToken:    stringPtr("│ "),
+		},
+		List: ansi.StyleList{
+			LevelIndent: defaultListIndent,
+		},
+		Heading: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				BlockSuffix: "\n",
+				Color:       stringPtr("39"),
+				Bold:        boolPtr(true),
+			},
+		},
+		H1: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				Prefix:          "🞙 ",
+				Suffix:          " 🞙",
+				Color:           stringPtr("39"),
+				BackgroundColor: stringPtr("63"),
+				Bold:            boolPtr(true),
+			},
+		},
+		H2: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				Prefix: "🞘🞘 ",
+				Suffix: " 🞘🞘",
+				Color:  stringPtr("38"),
+			},
+		},
+		H3: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				Prefix: "🞗🞗🞗 ",
+				Suffix: " 🞗🞗🞗",
+				Color:  stringPtr("40"),
+			},
+		},
+		H4: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				Prefix: "🞗🞗🞗🞗 ",
+				Color:  stringPtr("165"),
+			},
+		},
+		H5: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				Prefix: "🞗🞗🞗🞗🞗 ",
+				Suffix: " 🞗🞗🞗🞗🞗",
+				Color:  stringPtr("124"),
+			},
+		},
+		H6: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				Prefix: "🞗🞗🞗🞗🞗🞗 ",
+				Suffix: " 🞗🞗🞗🞗🞗🞗",
+				Color:  stringPtr("35"),
+				Bold:   boolPtr(false),
+			},
+		},
+		Strikethrough: ansi.StylePrimitive{
+			CrossedOut: boolPtr(true),
+		},
+		Emph: ansi.StylePrimitive{
+			Color:  stringPtr("245"),
+			Italic: boolPtr(true),
+		},
+		Strong: ansi.StylePrimitive{
+			Bold:  boolPtr(true),
+			Color: stringPtr("214"),
+		},
+		HorizontalRule: ansi.StylePrimitive{
+			Color:  stringPtr("240"),
+			Format: "\n----🝔----\n",
+		},
+		Item: ansi.StylePrimitive{
+			BlockPrefix: "🟆 ",
+		},
+		Enumeration: ansi.StylePrimitive{
+			BlockPrefix: ". ",
+		},
+		Task: ansi.StyleTask{
+			StylePrimitive: ansi.StylePrimitive{},
+			Ticked:         "[✓] ",
+			Unticked:       "[ ] ",
+		},
+		Link: ansi.StylePrimitive{
+			Color:     stringPtr("5"),
+			Underline: boolPtr(true),
+		},
+		LinkText: ansi.StylePrimitive{
+			Color: stringPtr("30"),
+			Bold:  boolPtr(true),
+		},
+		Image: ansi.StylePrimitive{
+			Color:     stringPtr("212"),
+			Underline: boolPtr(true),
+		},
+		ImageText: ansi.StylePrimitive{
+			Color:  stringPtr("243"),
+			Format: "Image: {{.text}} →",
+		},
+		Code: ansi.StyleBlock{
+			StylePrimitive: ansi.StylePrimitive{
+				Prefix:          " ",
+				Suffix:          " ",
+				Color:           stringPtr("203"),
+				BackgroundColor: stringPtr("236"),
+			},
+		},
+		CodeBlock: ansi.StyleCodeBlock{
+			StyleBlock: ansi.StyleBlock{
+				StylePrimitive: ansi.StylePrimitive{
+					Color: stringPtr("244"),
+				},
+				Margin: uintPtr(defaultMargin),
+			},
+			Chroma: &ansi.Chroma{
+				Text: ansi.StylePrimitive{
+					Color: stringPtr("#C4C4C4"),
+				},
+				Error: ansi.StylePrimitive{
+					Color:           stringPtr("#F1F1F1"),
+					BackgroundColor: stringPtr("#F05B5B"),
+				},
+				Comment: ansi.StylePrimitive{
+					Color: stringPtr("#676767"),
+				},
+				CommentPreproc: ansi.StylePrimitive{
+					Color: stringPtr("#FF875F"),
+				},
+				Keyword: ansi.StylePrimitive{
+					Color: stringPtr("#00AAFF"),
+				},
+				KeywordReserved: ansi.StylePrimitive{
+					Color: stringPtr("#FF5FD2"),
+				},
+				KeywordNamespace: ansi.StylePrimitive{
+					Color: stringPtr("#FF5F87"),
+				},
+				KeywordType: ansi.StylePrimitive{
+					Color: stringPtr("#6E6ED8"),
+				},
+				Operator: ansi.StylePrimitive{
+					Color: stringPtr("#EF8080"),
+				},
+				Punctuation: ansi.StylePrimitive{
+					Color: stringPtr("#E8E8A8"),
+				},
+				Name: ansi.StylePrimitive{
+					Color: stringPtr("#C4C4C4"),
+				},
+				NameBuiltin: ansi.StylePrimitive{
+					Color: stringPtr("#FF8EC7"),
+				},
+				NameTag: ansi.StylePrimitive{
+					Color: stringPtr("#B083EA"),
+				},
+				NameAttribute: ansi.StylePrimitive{
+					Color: stringPtr("#7A7AE6"),
+				},
+				NameClass: ansi.StylePrimitive{
+					Color:     stringPtr("#F1F1F1"),
+					Underline: boolPtr(true),
+					Bold:      boolPtr(true),
+				},
+				NameDecorator: ansi.StylePrimitive{
+					Color: stringPtr("#FFFF87"),
+				},
+				NameFunction: ansi.StylePrimitive{
+					Color: stringPtr("#00D787"),
+				},
+				LiteralNumber: ansi.StylePrimitive{
+					Color: stringPtr("#6EEFC0"),
+				},
+				LiteralString: ansi.StylePrimitive{
+					Color: stringPtr("#C69669"),
+				},
+				LiteralStringEscape: ansi.StylePrimitive{
+					Color: stringPtr("#AFFFD7"),
+				},
+				GenericDeleted: ansi.StylePrimitive{
+					Color: stringPtr("#FD5B5B"),
+				},
+				GenericEmph: ansi.StylePrimitive{
+					Italic: boolPtr(true),
+				},
+				GenericInserted: ansi.StylePrimitive{
+					Color: stringPtr("#00D787"),
+				},
+				GenericStrong: ansi.StylePrimitive{
+					Bold: boolPtr(true),
+				},
+				GenericSubheading: ansi.StylePrimitive{
+					Color: stringPtr("#777777"),
+				},
+				Background: ansi.StylePrimitive{
+					BackgroundColor: stringPtr("#373737"),
+				},
+			},
+		},
+		Table: ansi.StyleTable{
+			StyleBlock: ansi.StyleBlock{
+				StylePrimitive: ansi.StylePrimitive{},
+			},
+			CenterSeparator: stringPtr("┼"),
+			ColumnSeparator: stringPtr("│"),
+			RowSeparator:    stringPtr("─"),
+		},
+		DefinitionDescription: ansi.StylePrimitive{
+			BlockPrefix: "\n🠶 ",
+		},
+	}
+
+	r, _ := glamour.NewTermRenderer(
+		glamour.WithStandardStyle("dark"),
+		glamour.WithEmoji(), glamour.WithPreservedNewLines(),
+		glamour.WithWordWrap(width),
+		glamour.WithStyles(customStylingConfig),
+	)
+
 	for _, candidate := range resp.Candidates {
 		if candidate.Content != nil {
 			for _, part := range candidate.Content.Parts {
-				fmt.Println(part)
+				switch p := part.(type) {
+				case genai.Text:
+					out, err := r.Render(string(p))
+					fmt.Print(out)
+					if err != nil {
+						log.Fatal("error writing response: ", err)
+					}
+				case genai.Blob:
+					out, err := r.Render(fmt.Sprintf("Blob: MIMEType=%s, DataLength=%d", p.MIMEType, len(p.Data)))
+					if err != nil {
+						log.Fatal("error writing response: ", err)
+					}
+					fmt.Print(out)
+				}
 			}
 		}
 	}
-	fmt.Println()
-	fmt.Println("****************************************************************")
+	// fmt.Println(strings.Repeat("-", width-3))
+	fmt.Println(strings.Repeat("─", width-3), "\033[0;37m")
+	// fmt.Println(r.Render(string("---")))
+}
+
+func boolPtr(b bool) *bool {
+	return &b
+}
+
+func stringPtr(s string) *string {
+	return &s
+}
+
+func uintPtr(u uint) *uint {
+	return &u
+}
+
+func terminalWidth() int {
+	width, _, err := terminal.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		width = 100
+	}
+	return width
 }
